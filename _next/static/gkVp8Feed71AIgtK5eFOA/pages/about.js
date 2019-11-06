9f06997273282e4df960c14d7c9aa7d88ad79df0
VacantThinker <littlehappyforsmile@outlook.com>
2019-11-07 05:55:06 +0800

    Updates

--- _next/static/gkVp8Feed71AIgtK5eFOA/pages/about.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{Juyh:function(n,u,t){"use strict";t.r(u),t.d(u,"default",function(){return i});var o=t("h09O"),e=t("q1tI"),r=t.n(e).a.createElement;function i(){return r(o.a,null,r("div",null,"about page"))}},rB5V:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return t("Juyh")}])}},[["rB5V",1,0]]]);
